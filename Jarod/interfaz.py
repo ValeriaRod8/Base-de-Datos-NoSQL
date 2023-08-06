94b237d2d45991d1d20d38338b6b8e4656e9dc96
Jarod Santiago Viquez Salazar <jarod281@hotmail.com>
2023-08-06 16:36:59 -0600

    Proveedores
    Modulo de proveedores incluido al proyecto final

--- Jarod/interfaz.py
+++ Jarod/interfaz.py
@@ -127,7 +127,7 @@ def openWd_Articulo():
         descripcionArticulo.set(tupla[4])
         precioArticulo.set(tupla[5].removeprefix("$"))
         cantidadArticulo.set(tupla[6])
-        _idArticulo.set("Estas trabajando con el ID:  " + (str(idArticulo)))
+        _idArticulo.set("Trabajando el ID:  " + (str(idArticulo)))
         
         
 
@@ -207,7 +207,7 @@ def openWd_Articulo():
          global idArticulo
          limpiarCampos()
          idArticulo = ""
-         _idArticulo.set("Estas en modo creacion!!!!")
+         _idArticulo.set("Modo creacion")
     
     #Eliminar Regisrtro
     def eliminarRegistro():
@@ -250,6 +250,222 @@ def openWd_Articulo():
     btn_Eliminar = Button(f_Articulos,text="Eliminar", command = eliminarRegistro, bg ="#F58585", font=("",15)).place(x=428,y=823, width=100,height=50)
 
 
+def openWd_Proveedor():
+    
+    #Obtener la collecion a trabjar
+    seleccionCollecion("Proveedor")
+    global coleccion
+
+
+    wd_Proveedor = Toplevel(menuPrincipal)
+    wd_Proveedor.title('Floristeria - Articulos') #Nombre de la pagina
+    #wd_Articulos.iconbitmap('xxxx.ico') #Ver icono en ese momento ------
+
+
+    f_Proveedor = Frame(wd_Proveedor)
+    f_Proveedor.config(width = 1600, #1920
+                        heigh = 1024,#1080 
+                        bg ="#BCCCF3" )
+
+    f_Proveedor.pack(fill="both",expand="True")
+    
+    f_Proveedor.pack(fill="both",expand="True")
+
+    #Variables de Creacion, Edicion y Eliminacion
+    idproveedor = ""
+
+    #Variables de los text fields
+    IdProveedor = StringVar()
+
+    NombreProveedor = StringVar()
+    Apellidos = StringVar()
+    Provincia = StringVar()
+    Correo = StringVar()
+    Direccion = StringVar()
+    genero = StringVar()
+    IdTipoProveedor = StringVar()
+    _idProveedor = StringVar()
+
+
+    #Diseño de los widgets en wd_Articulos
+
+    #Label variable (Creacion o edicion/eliminacion)
+
+    lbl_Modulo = Label(f_Proveedor,text="Modulo de Proveedores", bg ="#BCCCF3", font=("",15)).place(x=144,y=75)
+
+    _idProveedor.set("Modo Creacion")
+
+
+    lbl_NombreProveedor = Label(f_Proveedor,textvariable=_idProveedor, bg ="#BCCCF3", font=("",15)).place(x=144,y=120)
+
+    #Labels and Text entries 
+    lbl_NombreProveedor = Label(f_Proveedor, text = "Nombre del Proveedor", bg ="#BCCCF3", font=("",15)).place(x=144,y=176)
+    txt_NombreProveedor = Entry(f_Proveedor,font=("",15),textvariable=NombreProveedor).place(x=144,y=206, width=385,height=36)
+
+    lbl_Apellidos = Label(f_Proveedor, text = "Apellidos", bg ="#BCCCF3", font=("",15)).place(x=144,y=265)
+    txt_Apellidos = Entry(f_Proveedor,font=("",15),textvariable= Apellidos).place(x=144,y=290, width=385,height=36)
+
+    lbl_Provincia = Label(f_Proveedor, text = "Provincia", bg ="#BCCCF3", font=("",15)).place(x=144,y=354)
+    txt_Provincia = Entry(f_Proveedor,font=("",15),textvariable=Provincia).place(x=144,y=379, width=385,height=36)
+
+    lbl_Correo = Label(f_Proveedor, text = "Correo", bg ="#BCCCF3", font=("",15)).place(x=144,y= 443)
+    txt_Correo  = Entry(f_Proveedor,font=("",15), textvariable=Correo).place(x=144,y=473,width=385,height=36)
+
+    lbl_Direccion = Label(f_Proveedor, text = "Direccion", bg ="#BCCCF3", font=("",15)).place(x=144,y=532)
+    txt_Direccion = Entry(f_Proveedor,font=("",15), textvariable=Direccion ).place(x=144,y=562, width=385,height=36)
+
+    lbl_Genero = Label(f_Proveedor, text = "Genero", bg ="#BCCCF3", font=("",15)).place(x=144,y=621)
+    txt_Genero = Entry(f_Proveedor,font=("",15), textvariable=genero).place(x=144,y=651, width=385,height=36)
+
+    lbl_tipoProveedor = Label(f_Proveedor, text = "Id Tipo Prove.", bg ="#BCCCF3", font=("",15)).place(x=144,y=710)
+    txt_tipoProveedor = Entry(f_Proveedor,font=("",15), textvariable=IdTipoProveedor).place(x=144,y=740, width=385,height=36)
+
+
+
+
+    #Extraer datos
+    def selectItem(a):
+        global idproveedor
+        curItem = tbl_Proveedores.focus()
+        tupla = tbl_Proveedores.item(curItem)['values']
+        #print(tupla)
+        idproveedor = tupla[0]
+        NombreProveedor.set(tupla[1])
+        Apellidos.set(tupla[2])
+        Provincia.set(tupla[3])
+        Correo.set(tupla[4])
+        Direccion.set(tupla[5])
+        genero.set(tupla[6])
+        IdTipoProveedor.set(tupla[7])
+        _idProveedor.set("Trabajando el ID:  " + (str(idproveedor)))
+        
+        
+
+    # Tabla
+    tbl_ProveedoresEstilo = ttk.Style()
+    tbl_ProveedoresEstilo.theme_use('clam')
+    tbl_ProveedoresEstilo.configure('tbl_Proveedoresview.Heading', background="#D9D9D9")
+
+    # Tabla Header
+    tbl_Proveedores= ttk.Treeview(f_Proveedor, column=("c1", "c2","c3","c4","c5","c6","c7","c8"), show= 'headings', height= 8)
+
+    tbl_Proveedores.column("# 1",anchor= CENTER, width=100)
+    tbl_Proveedores.heading("# 1", text= "Id Proveedor")
+    tbl_Proveedores.column("# 2",anchor= CENTER)
+    tbl_Proveedores.heading("# 2", text= "Nombre proveedor")
+    tbl_Proveedores.column("# 3", anchor= CENTER, width=50)
+    tbl_Proveedores.heading("# 3", text= "Apellidos")
+    tbl_Proveedores.column("# 4",anchor=CENTER, width=70)
+    tbl_Proveedores.heading("# 4", text= "Provincia")
+    tbl_Proveedores.column("# 5",anchor=CENTER)
+    tbl_Proveedores.heading("# 5", text= "Correo")
+    tbl_Proveedores.column("# 6",anchor=CENTER, width=70)
+    tbl_Proveedores.heading("# 6", text= "Direccion")
+    tbl_Proveedores.column("# 7",anchor=CENTER, width=70)
+    tbl_Proveedores.heading("# 7", text= "Genero")
+    tbl_Proveedores.column("# 8",anchor=CENTER, width=70)
+    tbl_Proveedores.heading("# 8", text= "Id Tipo proveedor")
+
+
+    tbl_Proveedores.bind('<ButtonRelease-1>', selectItem)
+
+    #Funcion para mostrar los datos
+    def mostrardatos():
+        try:
+            registros=tbl_Proveedores.get_children()
+            for registro in registros:
+                tbl_Proveedores.delete(registro)
+            for documento in coleccion.find():
+                tbl_Proveedores.insert('','end',text=documento["_id"],values=(documento["_id"],documento["NombreProveedor"],documento["Apellidos"],documento["Provincia"],documento["Correo"],documento["Direccion"],documento["Genero"],documento["IdTipoProveedor"]))
+            #cliente.server_info()
+            #print("Conexion a Mongo exitosa")
+            
+        except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
+            print("Tiempo extendido"+errorTiempo)
+        except pymongo.errors.ConectionFailure as errorConexion:
+            print("Fallo al conectarse a mongodb"+errorConexion) 
+
+    mostrardatos()
+
+    #Funcion crear registro
+    def crearRegistro():
+        global idArticulo
+        if len(NombreProveedor.get())!=0 and len(Apellidos.get())!=0 and len(Provincia.get())!=0 and len(Correo.get())!=0 and len(Direccion.get())!=0 and len(genero.get())!=0:
+
+                documento={"NombreProveedor": NombreProveedor.get(),
+                            "Apellidos": Apellidos.get(),
+                            "Provincia": Provincia.get(),
+                            "Correo" : Correo.get(),
+                            "Direccion":Direccion.get(), 
+                            "Genero":genero.get(), 
+                            "IdTipoProveedor":int(IdTipoProveedor.get())} 
+                coleccion.insert_one(documento)
+                refrescar()
+                idproveedor = ""
+        else:
+            messagebox.showerror(message="Los campos no pueden estar vacios")
+        mostrardatos()
+
+    # Limpiar Campos
+    def limpiarCampos():
+                
+                NombreProveedor.set('')
+                Apellidos.set('')
+                Provincia.set('')  
+                Correo.set('')  
+                Direccion.set('')   
+                genero.set('')  
+                IdTipoProveedor.set('')
+
+
+
+    #Refrescar Valores y setear en modo creacion
+    def refrescar():
+            global idproveedor
+            limpiarCampos()
+            idproveedor = ""
+            _idProveedor.set("Estas en el modulo de proveedores.")
+
+    #Eliminar Regisrtro
+    def eliminarRegistro():
+        global idproveedor
+        global coleccion
+        if (idproveedor!=""):
+            coleccion.delete_one({'_id': ObjectId(idproveedor)})
+            mostrardatos()
+            messagebox.showinfo(title="Eliminado",message='Proveedor con el ID:'+ str(idproveedor))
+            refrescar()
+        else:
+                messagebox.showerror(message='Debe seleccionar un registro')
+    #Actualizar Regisrtro
+
+    def actualizarRegistro():
+        if len(NombreProveedor.get())!=0 and len(Apellidos.get())!=0 and len(Provincia.get())!=0 and len(Correo.get())!=0 and len(Direccion.get())!=0 and len(genero.get())!=0:
+            global idproveedor
+            global coleccion
+            filter = { '_id': ObjectId(idproveedor) }
+            
+            newvalues = { "$set": { "NombreProveedor": NombreProveedor.get(),
+                            "Apellidos": Apellidos.get(),
+                            "Provincia": Provincia.get(),
+                            "Correo" : Correo.get(),
+                            "Direccion":Direccion.get(), 
+                            "Genero":genero.get(), 
+                            "IdTipoProveedor":int(IdTipoProveedor.get())}}
+
+            coleccion.update_one(filter, newvalues)
+            mostrardatos()
+            refrescar()
+        else:
+            messagebox.showerror(message='Los campos no pueden estar vacios')
+
+
+    #Ubicar la tabla en el frame
+    tbl_Proveedores.place(x=750,y=99, height=780)
+    btn_Ingresar = Button(f_Proveedor,text="Agregar", command=crearRegistro, bg ="#79C397", font=("",15)).place(x=144,y=798, width=100,height=50)
+    btn_EjecutarCambios = Button(f_Proveedor,text="Refrescar",command=refrescar, bg ="#7CA3EF", font=("",15)).place(x=428,y=798, width=100,height=50)
+    btn_Refrescar = Button(f_Proveedor,text="Editar", command=actualizarRegistro, bg ="#AAC213", font=("",15)).place(x=144,y=853, width=100,height=50)
+    btn_Eliminar = Button(f_Proveedor,text="Eliminar", command = eliminarRegistro, bg ="#F58585", font=("",15)).place(x=428,y=853, width=100,height=50)
 
 # Estilo del menu principal
 lbl_Floristeria = Label(f_menuPrincipal, text = "Floristeria Flores del Norte", bg ="#BCCCF3", font=("",44)).place(x=415,y=123)
@@ -274,11 +490,11 @@ def cambiarTamano(ancho,altura,imagen):
 #Imagenes y botones
 img = Image.open(obtenerImagen("Articulo","jpg"))
 imgobj_Articulo = ImageTk.PhotoImage(cambiarTamano(157,157,img))
-btn_Articulo = Button(f_menuPrincipal, text="Articulos", image=imgobj_Articulo, compound= TOP, font=("",15), command=openWd_Articulo).place(x=183,y=297, width=232,height=232)
+btn_Articulo = Button(f_menuPrincipal, text="Articulos", image=imgobj_Articulo, compound= TOP, font=("",15), command=openWd_Articulo).place(x=183,y=321, width=232,height=232)
 
 img = Image.open(obtenerImagen("Proveedores","jpg"))
 imgobj_Proveedores = ImageTk.PhotoImage(cambiarTamano(157,157,img))
-btn_Proveedores = Button(f_menuPrincipal, text="Proveedores", image=imgobj_Proveedores, compound= TOP, font=("",15) ).place(x=604,y=321, width=232,height=232)
+btn_Proveedores = Button(f_menuPrincipal, text="Proveedores", image=imgobj_Proveedores, compound= TOP, font=("",15), command=openWd_Proveedor ).place(x=604,y=321, width=232,height=232)
 
 img = Image.open(obtenerImagen("Clientes","jpg"))
 imgobj_Clientes = ImageTk.PhotoImage(cambiarTamano(157,157,img))
--- modulodeproveedores.py
+++ modulodeproveedores.py
@@ -48,6 +48,8 @@ f_Articulos.config(width = 1600, #1920
 
 f_Articulos.pack(fill="both",expand="True")
 '''
+
+
 #Variables de Creacion, Edicion y Eliminacion
 idproveedor = ""
 
@@ -67,35 +69,35 @@ _idProveedor = StringVar()
 #Diseño de los widgets en wd_Articulos
 
 #Label variable (Creacion o edicion/eliminacion)
-_idProveedor.set("Modulo de Proveedor")
+
+lbl_Modulo = Label(menuPrincipal,text="Modulo de Proveedores", bg ="#BCCCF3", font=("",15)).place(x=144,y=75)
+
+_idProveedor.set("Modo Creacion")
 
 
-lbl_NombreProveedor = Label(menuPrincipal,textvariable=_idProveedor, bg ="#BCCCF3", font=("",15)).place(x=144,y=75)
-
-
-lbl_NombreProveedor
+lbl_NombreProveedor = Label(menuPrincipal,textvariable=_idProveedor, bg ="#BCCCF3", font=("",15)).place(x=144,y=120)
 
 #Labels and Text entries 
-lbl_NombreArticulo = Label(menuPrincipal, text = "Nombre del Proveedor", bg ="#BCCCF3", font=("",15)).place(x=144,y=176)
-txt_NombreArticulo = Entry(menuPrincipal,font=("",15),textvariable=NombreProveedor).place(x=144,y=206, width=385,height=36)
+lbl_NombreProveedor = Label(menuPrincipal, text = "Nombre del Proveedor", bg ="#BCCCF3", font=("",15)).place(x=144,y=176)
+txt_NombreProveedor = Entry(menuPrincipal,font=("",15),textvariable=NombreProveedor).place(x=144,y=206, width=385,height=36)
 
-lbl_TipoArticulo = Label(menuPrincipal, text = "Apellidos", bg ="#BCCCF3", font=("",15)).place(x=144,y=265)
-txt_TipoArticulo = Entry(menuPrincipal,font=("",15),textvariable= Apellidos).place(x=144,y=290, width=385,height=36)
+lbl_Apellidos = Label(menuPrincipal, text = "Apellidos", bg ="#BCCCF3", font=("",15)).place(x=144,y=265)
+txt_Apellidos = Entry(menuPrincipal,font=("",15),textvariable= Apellidos).place(x=144,y=290, width=385,height=36)
 
-lbl_SucursalArticulo = Label(menuPrincipal, text = "Provincia", bg ="#BCCCF3", font=("",15)).place(x=144,y=354)
-txt_SucursalArticulo = Entry(menuPrincipal,font=("",15),textvariable=Provincia).place(x=144,y=379, width=385,height=36)
+lbl_Provincia = Label(menuPrincipal, text = "Provincia", bg ="#BCCCF3", font=("",15)).place(x=144,y=354)
+txt_Provincia = Entry(menuPrincipal,font=("",15),textvariable=Provincia).place(x=144,y=379, width=385,height=36)
 
-lbl_DescripcionArticulo = Label(menuPrincipal, text = "Correo", bg ="#BCCCF3", font=("",15)).place(x=144,y= 443)
-txt_DescripcionArticulo  = Entry(menuPrincipal,font=("",15), textvariable=Correo).place(x=144,y=473,width=385,height=36)
+lbl_Correo = Label(menuPrincipal, text = "Correo", bg ="#BCCCF3", font=("",15)).place(x=144,y= 443)
+txt_Correo  = Entry(menuPrincipal,font=("",15), textvariable=Correo).place(x=144,y=473,width=385,height=36)
 
-lbl_PrecioArticulo = Label(menuPrincipal, text = "Direccion", bg ="#BCCCF3", font=("",15)).place(x=144,y=532)
-txt_PrecioArticulo = Entry(menuPrincipal,font=("",15), textvariable=Direccion ).place(x=144,y=562, width=385,height=36)
+lbl_Direccion = Label(menuPrincipal, text = "Direccion", bg ="#BCCCF3", font=("",15)).place(x=144,y=532)
+txt_Direccion = Entry(menuPrincipal,font=("",15), textvariable=Direccion ).place(x=144,y=562, width=385,height=36)
 
-lbl_CantidadArticulo = Label(menuPrincipal, text = "Genero", bg ="#BCCCF3", font=("",15)).place(x=144,y=621)
-txt_CantidadArticulo = Entry(menuPrincipal,font=("",15), textvariable=genero).place(x=144,y=651, width=385,height=36)
+lbl_Genero = Label(menuPrincipal, text = "Genero", bg ="#BCCCF3", font=("",15)).place(x=144,y=621)
+txt_Genero = Entry(menuPrincipal,font=("",15), textvariable=genero).place(x=144,y=651, width=385,height=36)
 
-lbl_tipoproveedor = Label(menuPrincipal, text = "Id tipo Proveedor", bg ="#BCCCF3", font=("",15)).place(x=144,y=710)
-txt_tipoproveedor = Entry(menuPrincipal,font=("",15), textvariable=IdTipoProveedor).place(x=144,y=681, width=385,height=36)
+lbl_tipoProveedor = Label(menuPrincipal, text = "Id Tipo Prove.", bg ="#BCCCF3", font=("",15)).place(x=144,y=710)
+txt_tipoProveedor = Entry(menuPrincipal,font=("",15), textvariable=IdTipoProveedor).place(x=144,y=740, width=385,height=36)
 
 
 
@@ -103,8 +105,8 @@ txt_tipoproveedor = Entry(menuPrincipal,font=("",15), textvariable=IdTipoProveed
 #Extraer datos
 def selectItem(a):
     global idproveedor
-    curItem = tbl_Articulos.focus()
-    tupla = tbl_Articulos.item(curItem)['values']
+    curItem = tbl_Proveedores.focus()
+    tupla = tbl_Proveedores.item(curItem)['values']
     #print(tupla)
     idproveedor = tupla[0]
     NombreProveedor.set(tupla[1])
@@ -114,46 +116,46 @@ def selectItem(a):
     Direccion.set(tupla[5])
     genero.set(tupla[6])
     IdTipoProveedor.set(tupla[7])
-    _idProveedor.set("Estas trabajando con el ID:  " + (str(idproveedor)))
+    _idProveedor.set("Trabajando el ID:  " + (str(idproveedor)))
     
     
 
 # Tabla
-tbl_ArticuloEstilo = ttk.Style()
-tbl_ArticuloEstilo.theme_use('clam')
-tbl_ArticuloEstilo.configure('tbl_Articulosview.Heading', background="#D9D9D9")
+tbl_ProveedoresEstilo = ttk.Style()
+tbl_ProveedoresEstilo.theme_use('clam')
+tbl_ProveedoresEstilo.configure('tbl_Proveedoresview.Heading', background="#D9D9D9")
 
 # Tabla Header
-tbl_Articulos= ttk.Treeview(menuPrincipal, column=("c1", "c2","c3","c4","c5","c6","c7","c8"), show= 'headings', height= 8)
+tbl_Proveedores= ttk.Treeview(menuPrincipal, column=("c1", "c2","c3","c4","c5","c6","c7","c8"), show= 'headings', height= 8)
 
-tbl_Articulos.column("# 1",anchor= CENTER, width=100)
-tbl_Articulos.heading("# 1", text= "Id Proveedor")
-tbl_Articulos.column("# 2",anchor= CENTER)
-tbl_Articulos.heading("# 2", text= "Nombre proveedor")
-tbl_Articulos.column("# 3", anchor= CENTER, width=70)
-tbl_Articulos.heading("# 3", text= "Apellidos")
-tbl_Articulos.column("# 4",anchor=CENTER, width=70)
-tbl_Articulos.heading("# 4", text= "Provincia")
-tbl_Articulos.column("# 5",anchor=CENTER)
-tbl_Articulos.heading("# 5", text= "Correo")
-tbl_Articulos.column("# 6",anchor=CENTER, width=70)
-tbl_Articulos.heading("# 6", text= "Direccion")
-tbl_Articulos.column("# 7",anchor=CENTER, width=70)
-tbl_Articulos.heading("# 7", text= "Genero")
-tbl_Articulos.column("# 8",anchor=CENTER, width=70)
-tbl_Articulos.heading("# 8", text= "Id Tipo proveedor")
+tbl_Proveedores.column("# 1",anchor= CENTER, width=100)
+tbl_Proveedores.heading("# 1", text= "Id Proveedor")
+tbl_Proveedores.column("# 2",anchor= CENTER)
+tbl_Proveedores.heading("# 2", text= "Nombre proveedor")
+tbl_Proveedores.column("# 3", anchor= CENTER, width=70)
+tbl_Proveedores.heading("# 3", text= "Apellidos")
+tbl_Proveedores.column("# 4",anchor=CENTER, width=70)
+tbl_Proveedores.heading("# 4", text= "Provincia")
+tbl_Proveedores.column("# 5",anchor=CENTER)
+tbl_Proveedores.heading("# 5", text= "Correo")
+tbl_Proveedores.column("# 6",anchor=CENTER, width=70)
+tbl_Proveedores.heading("# 6", text= "Direccion")
+tbl_Proveedores.column("# 7",anchor=CENTER, width=70)
+tbl_Proveedores.heading("# 7", text= "Genero")
+tbl_Proveedores.column("# 8",anchor=CENTER, width=70)
+tbl_Proveedores.heading("# 8", text= "Id Tipo proveedor")
 
 
-tbl_Articulos.bind('<ButtonRelease-1>', selectItem)
+tbl_Proveedores.bind('<ButtonRelease-1>', selectItem)
 
 #Funcion para mostrar los datos
 def mostrardatos():
     try:
-        registros=tbl_Articulos.get_children()
+        registros=tbl_Proveedores.get_children()
         for registro in registros:
-            tbl_Articulos.delete(registro)
+            tbl_Proveedores.delete(registro)
         for documento in coleccion.find():
-            tbl_Articulos.insert('','end',text=documento["_id"],values=(documento["_id"],documento["NombreProveedor"],documento["Apellidos"],documento["Provincia"],documento["Correo"],documento["Direccion"],documento["Genero"],documento["IdTipoProveedor"]))
+            tbl_Proveedores.insert('','end',text=documento["_id"],values=(documento["_id"],documento["NombreProveedor"],documento["Apellidos"],documento["Provincia"],documento["Correo"],documento["Direccion"],documento["Genero"],documento["IdTipoProveedor"]))
         #cliente.server_info()
         #print("Conexion a Mongo exitosa")
         
@@ -238,10 +240,10 @@ def actualizarRegistro():
 
 
 #Ubicar la tabla en el frame
-tbl_Articulos.place(x=750,y=99, height=780)
-btn_Ingresar = Button(menuPrincipal,text="Agregar", command=crearRegistro, bg ="#79C397", font=("",15)).place(x=144,y=738, width=100,height=50)
-btn_EjecutarCambios = Button(menuPrincipal,text="Refrescar",command=refrescar, bg ="#7CA3EF", font=("",15)).place(x=428,y=738, width=100,height=50)
-btn_Refrescar = Button(menuPrincipal,text="Editar", command=actualizarRegistro, bg ="#AAC213", font=("",15)).place(x=144,y=823, width=100,height=50)
-btn_Eliminar = Button(menuPrincipal,text="Eliminar", command = eliminarRegistro, bg ="#F58585", font=("",15)).place(x=428,y=823, width=100,height=50)
+tbl_Proveedores.place(x=750,y=99, height=780)
+btn_Ingresar = Button(menuPrincipal,text="Agregar", command=crearRegistro, bg ="#79C397", font=("",15)).place(x=144,y=798, width=100,height=50)
+btn_EjecutarCambios = Button(menuPrincipal,text="Refrescar",command=refrescar, bg ="#7CA3EF", font=("",15)).place(x=428,y=798, width=100,height=50)
+btn_Refrescar = Button(menuPrincipal,text="Editar", command=actualizarRegistro, bg ="#AAC213", font=("",15)).place(x=144,y=853, width=100,height=50)
+btn_Eliminar = Button(menuPrincipal,text="Eliminar", command = eliminarRegistro, bg ="#F58585", font=("",15)).place(x=428,y=853, width=100,height=50)
 
 mainloop()
